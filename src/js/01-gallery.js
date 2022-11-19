d816e6c3584769e791d378e31aa0f52ecd6efb3c
ruchey34 <me.oleg1@gmail.com>
2022-11-19 14:18:03 +0200

    1 - 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkUp = createGalleryMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkUp);
+
+// paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+
+ function createGalleryMarkup(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+        `;
+      })
+      .join('');
+      
+      
+  }
+  const lightbox = new SimpleLightbox('.gallery a', { captionsData:"alt", captionDelay:250});
